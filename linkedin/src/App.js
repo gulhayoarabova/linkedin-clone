93bbdf0c9671fb8713557828e84f1e7d54f279e1
gulhayoarabova <gulhayoarabova@gmail.com>
2022-09-28 19:12:49 +0500

    login page completed

---
+++ linkedin/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import {BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+    <Router>
+      <Routes>
+        <Route  path="/" element={<Login/>}>
+        </Route>
+      </Routes>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
