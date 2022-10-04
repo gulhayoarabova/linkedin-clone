33e6f12d8a1112d1af01a0d775cac46f8aa8249c
gulhayoarabova <gulhayoarabova@gmail.com>
2022-10-04 19:32:55 +0500

    left side has been completed

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import {BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route  path="/" element={<Login/>}>
+        </Route>
+        <Route path="/home" element={<Home/>} >
         </Route>
       </Routes>
     </Router>
